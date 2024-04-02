dcc99c033faef52031197074024f0909f04599c9
myownl97 <joancasal97@gmail.com>
2024-04-02 16:21:14 +0200

    Agregado splashScreen. Añadido un activity y modificado el manifest para que sea el activity inicial.

---
+++ app/src/main/java/com/example/myapplication/SplashcreenActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+import android.os.Bundle;
+
+public class SplashcreenActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 2000; // 2 segundos
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashcreen);
+
+        // Retrasar la apertura de LoginActivity durante 2 segundos
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Este código se ejecutará después de SPLASH_DELAY milisegundos
+
+                // Abrir LoginActivity
+                Intent intent = new Intent(SplashcreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Finalizar la actividad actual
+            }
+        }, SPLASH_DELAY);
+    }
+}
