37286e6dfb569b0ea9fe126de2c9a427de4f39a7
Ismael <hernandezhernandezismael@gmail.com>
2024-04-04 22:07:18 +0100

    Añado boton todas a listview

--- app/src/main/java/com/example/myapplication/SelecPlataforma.java
+++ app/src/main/java/com/example/myapplication/SelecPlataforma.java
@@ -19,28 +19,15 @@ public class SelecPlataforma extends AppCompatActivity {
         setContentView(R.layout.activity_plataforma);
 
         lista = (ListView)findViewById(R.id.list_plata);
-        btnTodas = findViewById(R.id.btn_todas);
+
         // Obtener tipo de estreno seleccionado (estrenados o proximos)
         String tipoEstreno = getIntent().getStringExtra("tipoEstreno");
 
-        String[] itemNames = {"Netflix", "HBO", "Disney", "Prime"};
-        Integer[] itemImages = {R.drawable.logo_netflix_removebg_preview, R.drawable.logo_hbo_removebg_preview, R.drawable.logo_disney1_removebg_preview, R.drawable.logo_prime_removebg_preview};
+        String[] itemNames = {"Netflix", "HBO", "Disney", "Prime", "todas"};
+        Integer[] itemImages = {R.drawable.logo_netflix_removebg_preview, R.drawable.logo_hbo_removebg_preview, R.drawable.logo_disney1_removebg_preview, R.drawable.logo_prime_removebg_preview, R.drawable.botontodas};
 
         list_plataformas_adaptador adapter = new list_plataformas_adaptador(this, itemNames, itemImages, tipoEstreno);
         lista.setAdapter(adapter);
-        btnTodas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mostrarTodas(); // Llamar al método mostrarTodas
-            }
-        });
-    }
-
-    private void mostrarTodas() {
-        Intent intent = new Intent(SelecPlataforma.this, PlataformaActivity.class);
-        intent.putExtra("tipoEstreno", getIntent().getStringExtra("tipoEstreno"));
-        intent.putExtra("plataforma", "todas");
-        startActivity(intent);
     }
 
     public void atras (View view){
